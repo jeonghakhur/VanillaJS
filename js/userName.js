ad554ac67459c453b82e7d47d24b4a31f351a677
inpix <jeonghak@inpix.com>
2018-12-15 21:44:19 +0900

    localStorage

--- js/userName.js
+++ js/userName.js
@@ -1,8 +1,39 @@
 const form = document.querySelector('.user-name');
 const input = form.querySelector('input');
+const greeting = document.querySelector('.greeting');
+
+
+const USER_LS = 'currentUser';
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const currentValue = input.value;
+  localStorage.setItem(USER_LS, currentValue);
+  input.value = '';
+}
+
+function askForName() {
+  form.classList.add('show');
+  form.addEventListener('submit', handleSubmit);
+}
+
+function paintGreeting(text) {
+  form.classList.remove('show');
+  greeting.classList.add('show');
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser)
+  }
+}
 
 function init() {
-
+  loadName();
 }
 
 init()
