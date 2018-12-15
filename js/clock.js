6e1d69460cd408a726bd725a0ad06ed49d7aba4f
inpix <jeonghak@inpix.com>
2018-12-15 20:58:34 +0900

    mm

--- js/clock.js
+++ js/clock.js
@@ -3,13 +3,6 @@ const clockTitle = clockContainer.querySelector('h1');
 
 function getTime() {
   const date = new Date();
-  // date.setHours(1);
-  // date.setMinutes(1);
-  // const minutes = date.getMinutes();
-  // const hours = date.getHours();
-  // const seconds = date.getSeconds();
-  // clockTitle.innerText = `${hours}:${minutes}:${seconds}`
-  // clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
   const minutes = lengthCheck(date.getMinutes(), 10);
   const hours = lengthCheck(date.getHours(), 10);
   const seconds = lengthCheck(date.getSeconds(), 10);
