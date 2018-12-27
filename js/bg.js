4cf1ba64c34832b4db7b479abb8f5faaa61f9dd7
jeonghak <jeonghak.jur@gmail.com>
2018-12-27 15:28:36 +0900

    master

--- js/bg.js
+++ js/bg.js
@@ -1,12 +1,21 @@
 const bgImage = document.querySelector('.bg-image');
 
-const IMG_NUMBER = 3;
+const IMG_NUMBER = 7;
+
+function handleImgLoad(image) {
+  bgImage.style.backgroundImage = `url('${image.src}')`;
+  bgImage.classList.add('fadeIn');
+}
 
 function paintImage() {
   const image = new Image();
   image.src = `./img/${getRandom(1, IMG_NUMBER)}.jpg`;
-  bgImage.style.backgroundImage = `url('${image.src}')`;
-  bgImage.classList.add('fadeIn');
+  // bgImage.style.backgroundImage = `url('${image.src}')`;
+  // bgImage.classList.add('fadeIn');
+  image.addEventListener('load', function() {
+    handleImgLoad(image)
+  });
+
 }
 
 // min (포함) 과 max (포함) 사이의 임의 정수를 반환
--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,11 @@
 const weather = document.querySelector('.weather');
 //https://home.openweathermap.org/
 const API_KEY = 'eebedd7fbd8382cd1f41f22ae440820e'
+
+// 1.1
 const COORDS = 'coords';
 
+// 6.1
 function getWeather(lat, log) {
   // fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}`);
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`)
@@ -11,17 +14,19 @@ function getWeather(lat, log) {
     return response.json();
   })
   .then(function(json) {
-    // console.log(json);
+    console.log(json);
     const temperature = json.main.temp;
-    const place = json.name;
+    const place = json.name
     weather.innerText = `${temperature} @ ${place}`;
   })
 }
 
+// 5.1
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
+// 3.
 function handleGeoSuccess(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
@@ -29,18 +34,25 @@ function handleGeoSuccess(position) {
     latitude,
     longitude
   }
+
+  // 5.
   saveCoords(coordsObj);
+  // 6
   getWeather(latitude, longitude);
 }
 
+
+// 4.
 function handleGeoError() {
   console.log('Cant access geo location')
 }
 
+// 2.
 function askForCoords() {
   navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
 }
 
+// 1.
 function loadCoords() {
   const loadedCoords = localStorage.getItem(COORDS);
   if (loadedCoords === null) {
